23aac1ea617e9fecdb57875b3196d9cf9c3a187d
guliguliaffe <guliguliaffe@gmail.com>
2024-07-05 10:51:19 +0000

    ok

--- components/MyFirstComponent.vue
+++ components/MyFirstComponent.vue
@@ -1,5 +1,72 @@
 <template>
-  <h1>Hello world</h1>
+
+
+  <v-expansion-panels>
+    <v-expansion-panel>
+      <v-expansion-panel-title v-slot="{ }">
+        <v-row no-gutters>
+          <v-col align="left" >
+  
+          <v-img
+            width="50px"
+            aspect-ratio="16/9"
+            align="center"
+           :src="props.teamOneIcon"
+           >
+          </v-img>
+          </v-col>
+            <v-col align="left"> {{props.teamOne}} vs {{props.teamTwo}}</v-col>
+            <v-col align="left">
+              <v-img  
+                width="50px"
+                aspect-ratio="16/9"
+                align="center"
+                :src="props.teamTwoIcon"
+                >
+              </v-img>
+            </v-col>
+            <v-col align ="right">{{props.matchTime}}</v-col>
+        </v-row>
+      </v-expansion-panel-title>
+      <v-expansion-panel-text>
+        <v-table theme="light">
+          <thead>
+            <tr>
+              <th class="text-left">Ergebnis</th>
+              <th class="text-left"></th>
+              <th class="text-left">Torschütze</th>
+              <th class="text-left">Minute</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="item in Spiel" :key="item.ergebnis">
+              <th>{{ item.leer }}</th>
+              <td>{{ item.ergebnis }}</td>
+              <td>{{ item.torschuetze }}</td>
+              <td>{{ item.minute }}</td>
+            </tr>
+          </tbody>
+        </v-table>
+      </v-expansion-panel-text>
+    </v-expansion-panel>
+  </v-expansion-panels>
 </template>
 
-<style></style>
+<script setup>
+const props = defineProps(['teamOne', "teamTwo", "teamOneIcon", "teamTwoIcon", "matchTime"])
+</script>
+
+<script>
+export default {
+   data() { return {
+     Spiel: [ 
+      {
+          ergebnis: 'null',
+          torschuetze:'null',
+  	      minute: 'null', 
+      },
+            ], 
+          }
+        },
+      }
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,58 @@
 <template>
+
+
   <v-container class="fill-height">
     <v-row no-gutters>
       <v-col cols="12" md="12" lg="12" sm="12">
-        <my-first-component></my-first-component>
+        <my-first-component v-for="match in groups.firstGroupStage" :key="match.matchID" :team-one="match.team1.teamName" :team-two="match.team2.teamName" :team-one-icon="match.team1.teamIconUrl" :team-two-icon="match.team2.teamIconUrl" :match-time="match.matchDateTime"></my-first-component>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script setup>
-//const { data } = await useFetch("https://api.openligadb.de/getmatchdata/em/2024/");
+
+const { data } = await useFetch(
+  "https://api.openligadb.de/getmatchdata/em/2024/"
+);
+
+ let groups = {
+   firstGroupStage: [],
+   secondGroupStage: [],
+   thirdGroupStage: [],
+   roundOf16: [],
+   roundOf8: [],
+   roundOf4: [],
+   final: [],
+ };
+ 
+ for (const match of data.value) {
+   if (match.group.groupName === "1. Runde Gruppenphase") {
+     groups.firstGroupStage.push(match)
+   }
+   if (match.group.groupName === "2. Runde Gruppenphase") {
+     groups.secondGroupStage.push(match)
+   }
+   if (match.group.groupName === "3. Runde Gruppenphase") {
+     groups.thirdGroupStage.push(match)
+   }
+   if (match.group.groupName === "Achtelfinale") {
+     groups.roundOf16.push(match)
+   }
+   if (match.group.groupName === "Viertelfinale") {
+     groups.roundOf8.push(match)
+   }
+   if (match.group.groupName === "Halbfinale") {
+     groups.roundOf4.push(match)
+   }
+   if (match.group.groupName === "Finale") {
+     groups.final.push(match)
+   }
+ }
+
+
+
+
 </script>
+
+
