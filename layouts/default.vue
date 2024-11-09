36de998ffbe53a6c83d5477c483e38c8374f6b1c
guliguliaffe <guliguliaffe@gmail.com>
2024-11-09 14:10:08 +0000

    .

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,14 @@
+
+
 <template>
   <div>
     <VApp>
       <VMain>
-        <v-app-bar class="top-bar" density="compact" color="#7986CB">
-          <template v-slot:prepend>
-            <img src="/gak-logo.png" alt="" class="gak-logo" />
-          </template>
 
-          <v-app-bar-title>UEFA EURO 2024</v-app-bar-title>
 
-          <template v-slot:append>
-            <v-btn icon="mdi-help-circle-outline" size="large"></v-btn>
-          </template>
-        </v-app-bar>
+         <slot/> 
 
-        <slot />
+
       </VMain>
     </VApp>
   </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,58 +1,99 @@
 <template>
+  <v-card max-width="1400" class="mx-auto">
+    <v-system-bar color="pink darken-2" dark>
+    </v-system-bar>
 
+    <v-app-bar dark color="pink">
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>My Audiothek</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+    </v-app-bar>
 
-  <v-container class="fill-height">
-    <v-row no-gutters>
-      <v-col cols="12" md="12" lg="12" sm="12">
-        <my-first-component v-for="match in groups.firstGroupStage" :key="match.matchID" :team-one="match.team1.teamName" :team-two="match.team2.teamName" :team-one-icon="match.team1.teamIconUrl" :team-two-icon="match.team2.teamIconUrl" :match-time="match.matchDateTime"></my-first-component>
-      </v-col>
-    </v-row>
-  </v-container>
+    <!-- Card Carousel Section -->
+    <v-sheet class="overflow-hidden" height="220">
+      <v-slide-group
+        v-model="selectedSlide"
+        class="pa-4"
+        show-arrows
+        mandatory
+      >
+        <v-slide-item
+          v-for="(item, i) in items"
+          :key="i"
+        >
+          <v-card
+            :color="item.color"
+            dark
+            class="ma-2"
+            width="300"
+            @click="selectSlide(i)"
+          >
+            <div class="d-flex flex-no-wrap justify-space-between">
+              <div>
+                <v-card-title class="text-h5" v-text="item.title"></v-card-title>
+                <v-card-subtitle v-text="item.artist"></v-card-subtitle>
+                <v-card-actions>
+                  <v-btn
+                    class="ml-2 mt-3"
+                    fab
+                    icon
+                    height="40px"
+                    right
+                    width="40px"
+                  >
+                    <v-icon>mdi-download</v-icon>
+                  </v-btn>
+                </v-card-actions>
+              </div>
+              <v-avatar class="ma-3" size="125" tile>
+                <v-img :src="item.src"></v-img>
+              </v-avatar>
+            </div>
+          </v-card>
+        </v-slide-item>
+      </v-slide-group>
+    </v-sheet>
+  </v-card>
 </template>
 
-<script setup>
-
-const { data } = await useFetch(
-  "https://api.openligadb.de/getmatchdata/em/2024/"
-);
-
- let groups = {
-   firstGroupStage: [],
-   secondGroupStage: [],
-   thirdGroupStage: [],
-   roundOf16: [],
-   roundOf8: [],
-   roundOf4: [],
-   final: [],
- };
- 
- for (const match of data.value) {
-   if (match.group.groupName === "1. Runde Gruppenphase") {
-     groups.firstGroupStage.push(match)
-   }
-   if (match.group.groupName === "2. Runde Gruppenphase") {
-     groups.secondGroupStage.push(match)
-   }
-   if (match.group.groupName === "3. Runde Gruppenphase") {
-     groups.thirdGroupStage.push(match)
-   }
-   if (match.group.groupName === "Achtelfinale") {
-     groups.roundOf16.push(match)
-   }
-   if (match.group.groupName === "Viertelfinale") {
-     groups.roundOf8.push(match)
-   }
-   if (match.group.groupName === "Halbfinale") {
-     groups.roundOf4.push(match)
-   }
-   if (match.group.groupName === "Finale") {
-     groups.final.push(match)
-   }
- }
-
-
-
-
+<script>
+export default {
+  data: () => ({
+    selectedSlide: 0,
+    items: [
+      {
+        color: '#1F7087',
+        src: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+        title: 'Supermodel',
+        artist: 'Foster the People',
+      },
+      {
+        color: '#952175',
+        src: 'https://cdn.vuetifyjs.com/images/cards/halcyon.png',
+        title: 'Halcyon Days',
+        artist: 'Ellie Goulding',
+      },
+      {
+        color: '#1F7087',
+        src: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+        title: 'Another Album',
+        artist: 'Another Artist',
+      },
+      {
+        color: '#952175',
+        src: 'https://cdn.vuetifyjs.com/images/cards/halcyon.png',
+        title: 'Fourth Album',
+        artist: 'Fourth Artist',
+      },
+    ],
+  }),
+  methods: {
+    selectSlide(index) {
+      this.selectedSlide = index;
+    }
+  },
+}
 </script>
-
-
